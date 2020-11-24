bfed8f3018ba05e19c4c0293211f7ba4c5b60865
user-lu <user.sevilu6@gmail.com>
2020-11-24 14:02:57 -0600

    changed the function to be more organized, added questions and prompts to determine the random password

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -10,21 +9,54 @@ function writePassword() {
     passwordText.value = password;
     
 }
+// The characters needed to make the random password
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "1234567890";
+let specialChar = "!@#$%^&*():;?{}";
 
 function generatePassword() {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz";
-    // let upperCase = alphabet.toUpperCase();
-    // let numbers = "123456789";
-    // let specialChar = "!@#$%^&*():;?{}";
-    randomPassword = "";
-
-    for (let i = 0; i < 15; i++) {
-        var randomChar = Math.floor(Math.random() * alphabet.length); 
-        randomPassword += alphabet.charAt(randomChar)
-
-    }
     
-    return randomPassword;
+    let newPassword = "";
+    let passwordSet = "";
+
+// Asks user how many characters they want their password to be
+    let inputLength = prompt("How many characters would you like?");
+
+    if (Number(inputLength < 8) | Number(inputLength > 128)) {
+        alert("Enter a number between 8 and 128");
+    }
+
+    let lowerCaseInput = confirm("Do you want lowercase letters?");
+
+    if (lowerCaseInput) {
+        passwordSet += lowerCase;
+    }
+
+    let upperCaseInput = confirm("Do you want uppercase letters?");
+
+    if (upperCaseInput) {
+        passwordSet += upperCase;
+    }
+    let numberInput = confirm("Do you want numbers?");
+
+    if (numberInput) {
+        passwordSet += numbers;
+    }
+    let specialCharInput = confirm("Do you want special characters?");
+
+    if (specialCharInput) {
+        passwordSet += specialChar;
+    }
+
+    if (Number(inputLength >= 8) | Number(inputLength <= 128)) {
+     for (let i = 0; i < inputLength; i++) {
+         newPassword += passwordSet.charAt(Math.floor(Math.random() * passwordSet.length));      
+     };   
+    }
+
+    return newPassword;
+    
 }
 
 // Add event listener to generate button
